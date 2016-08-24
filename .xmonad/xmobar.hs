afda9a8f2358d1ba8760366df1b61b26f08ea3cf
Richard Stewing <richy.sting@gmail.com>
2016-08-24 14:43:03 -0700

    xmonad config

---
+++ .xmonad/xmobar.hs
@@ -0,0 +1,23 @@
+Config { borderColor = "black"
+       , border = TopB
+       , bgColor = "black"
+       , fgColor = "grey"
+       , alpha = 255
+       , position = Top
+       , textOffset = -1
+       , iconOffset = -1
+       , lowerOnStart = True
+       , pickBroadest = False
+       , persistent = False
+       , hideOnStart = False
+       , iconRoot = "."
+       , allDesktops = True
+       , overrideRedirect = True
+       , sepChar = "%"
+       , alignSep = "}{"
+       
+       , commands = [
+           Run Com "/home/haetze/.battery" [] "battery" 600
+           ]
+       , template = "<fc=#ee9a00>%date%</fc>| %uname% | %battery%"
+       }
---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,21 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+
+main = do
+    xmproc <- spawnPipe "/home/haetze/.cabal/bin/xmobar /home/haetze/.xmonad/xmobar.hs"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        } `additionalKeys`
+        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
