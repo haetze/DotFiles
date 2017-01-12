87c8566c15b46028a68e4c681305673481ba5f27
Richard Stewing <richy.sting@gmail.com>
2017-01-12 07:59:35 -0800

    additons to xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,8 +18,11 @@ main = do
         } `additionalKeys`
         [ ((mod1Mask, xK_o), spawn "xtrlock")
         , ((mod1Mask, xK_s), spawn "scrot '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/shots/'" )
+        , ((mod1Mask, xK_u), spawn "scrot '%Y-%m-%d_%H:%M:%S.png' -s -e 'mv $f ~/shots/'" )
         , ((mod1Mask, xK_F1), spawn "/home/haetze/usefulCommands/1live.sh")
         , ((mod1Mask, xK_F2), spawn "pkill mpg123")
         , ((mod1Mask, xK_F12), spawn "sysctl hw.snd.default_unit=1")
         , ((mod1Mask, xK_F11), spawn "sysctl hw.snd.default_unit=0")
+        , ((mod1Mask, xK_F9), spawn "amixer set Master 1%-")
+        , ((mod1Mask, xK_F10), spawn "amixer set Master 1%+")
         ]
