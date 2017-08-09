8eb8f32313cca93bab373ec0a0fdfdf812e71f21
Richard Stewing <richy.sting@gmail.com>
2017-08-09 07:22:19 -0700

    updated xmobar.hs

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -14,14 +14,38 @@ Config { borderColor = "black"
        , allDesktops = True
        , overrideRedirect = True
        , sepChar = "%"
+       , additionalFonts = []
+       , font = "xft:Bitstream Vera Sans Mono:size=9:bold:anitalias=true"
        , alignSep = "}{"
        
        , commands = [
-           Run Com "/home/haetze/.battery" [] "battery" 60,
-           Run Com "/home/haetze/.battery2" [] "state" 60,
-           Run Com "/home/haetze/.network" [] "network" 60,
-           Run Com "/home/haetze/.mem.sh" [] "mem" 60,
-           Run Com "/home/haetze/.audio" [] "audio" 60
+            Run Memory [ "-t"       , "Mem: <usedratio>%"
+                       , "--low"    , "darkgreen"
+                       , "--normal" , "darkorange"
+                       , "--high"   , "darkred"
+                       ] 10
+           ,Run Swap [] 10
+           ,Run Cpu [ "-L"       , "3"
+                    , "-H"       , "50"
+                    , "--normal" , "green"
+                    , "--high"   , "red"
+                   ] 10
+           ,Run Battery [ "--template" , "Batt: <acstatus>"
+                        , "--low"      , "darkred"
+                        , "--normal"   , "darkorange"
+                        , "--high"     , "darkgreen"
+                        , "--"
+                        , "-o"         , "<left>% (<timeleft>)"
+                        , "-O"         , "<fc=#dAA520>Charging</fc>"
+                        , "-i"         , "<fc=#006000>Charged</fc>"
+                        ] 10
+           ,Run DynNetwork [ "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
+                           , "--low"      , "darkgreen"
+                           , "--normal"   , "darkorange"
+                           , "--high"     , "darkred"
+                           ] 10
+           ,Run Weather "EDLW" [ "--template" , "<skyCondition> | <fc=#4682B4><tempC></fc>C"
+                               ] 3600
            ]
-       , template = "<fc=#ee9a00>%date%</fc>| %uname% | %battery% | %state% | %network% | %audio% | %mem% |"
+       , template = "<fc=#ee9a00>%date%</fc>| %uname% | %memory% | %dynnetwork% | %cpu% | %battery% | %EDLW% "
        }
