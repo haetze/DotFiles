8dbba144ca91df1f88ad825c73cbd49173a128af
Richard Stewing <richy.sting@gmail.com>
2017-12-21 15:48:11 +0100

    changes

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -15,37 +15,12 @@ Config { borderColor = "black"
        , overrideRedirect = True
        , sepChar = "%"
        , additionalFonts = []
-       , font = "xft:Bitstream Vera Sans Mono:size=9:bold:anitalias=true"
+       , font = "xft:Bitstream Vera Sans Mono:size=18:bold:anitalias=true"
        , alignSep = "}{"
        
        , commands = [
-            Run Memory [ "-t"       , "Mem: <usedratio>%"
-                       , "--low"    , "darkgreen"
-                       , "--normal" , "darkorange"
-                       , "--high"   , "darkred"
-                       ] 10
-           ,Run Swap [] 10
-           ,Run Cpu [ "-L"       , "3"
-                    , "-H"       , "50"
-                    , "--normal" , "green"
-                    , "--high"   , "red"
-                   ] 10
-           ,Run Battery [ "--template" , "Batt: <acstatus>"
-                        , "--low"      , "darkred"
-                        , "--normal"   , "darkorange"
-                        , "--high"     , "darkgreen"
-                        , "--"
-                        , "-o"         , "<left>% (<timeleft>)"
-                        , "-O"         , "<fc=#dAA520>Charging</fc>"
-                        , "-i"         , "<fc=#006000>Charged</fc>"
-                        ] 10
-           ,Run DynNetwork [ "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
-                           , "--low"      , "darkgreen"
-                           , "--normal"   , "darkorange"
-                           , "--high"     , "darkred"
-                           ] 10
-           ,Run Weather "EDLW" [ "--template" , "<skyCondition> | <fc=#4682B4><tempC></fc>C"
-                               ] 3600
+           Run Com "/home/haetze/usefulCommands/.battery" [] "battery" 10
+           ,Run Com "/home/haetze/usefulCommands/.audio" [] "audio" 10
            ]
-       , template = "<fc=#ee9a00>%date%</fc>| %uname% | %memory% | %dynnetwork% | %cpu% | %battery% | %EDLW% "
+       , template = "<fc=#ee9a00>%date%</fc>| %uname% | %battery% | %audio%"
        }
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Core
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe, runInTerm)
 import XMonad.Util.EZConfig(additionalKeys)
@@ -18,11 +19,11 @@ main = do
         } `additionalKeys`
         [ ((mod1Mask, xK_o), spawn "xtrlock")
         , ((mod1Mask, xK_s), spawn "scrot '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/shots/'" )
-        , ((mod1Mask, xK_u), spawn "scrot '%Y-%m-%d_%H:%M:%S.png' -s -e 'mv $f ~/shots/'" )
+        , ((mod1Mask, xK_u), spawn "xterm" )
         , ((mod1Mask, xK_F1), spawn "/home/haetze/usefulCommands/1live.sh")
         , ((mod1Mask, xK_F2), spawn "pkill mpg123")
-        , ((mod1Mask, xK_F12), spawn "sysctl hw.snd.default_unit=1")
-        , ((mod1Mask, xK_F11), spawn "sysctl hw.snd.default_unit=0")
+        , ((mod1Mask, xK_F12), spawn "sysctl hw.snd.default_unit=6")
+        , ((mod1Mask, xK_F11), spawn "sysctl hw.snd.default_unit=4")
         , ((mod1Mask, xK_F9), spawn "amixer set Master 1%-")
         , ((mod1Mask, xK_F10), spawn "amixer set Master 1%+")
         ]
